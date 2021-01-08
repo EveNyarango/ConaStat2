01214dd51041d4f26807a5682cdf1e35a2f3487e
EveNyarango <evenyarango@gmail.com>
2021-01-08 12:49:43 +0300

    create arrayAdapter Activity

--- app/src/main/java/com/example/myconastat/MainActivity.java
+++ app/src/main/java/com/example/myconastat/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.myconastat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+@BindView(R.id.continentTextView) TextView mContinentTextView;
+@BindView(R.id.listview)  ListView mListView;
+private String[] countries = new String[] {"China", "Us", "Vietnam", "United Kingdom", "United Arab Emirates", "Ghana", "Kenya", "France", "Mexico", "Australia"};
+private String[] continents = new String[] {"Asia", "North America", "Asia","Europe", "Asia", "Africa", "Africa", "Europe", "North America", "Oceania"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mListView = (ListView) findViewById(R.id.listview);
+        mContinentTextView = (TextView) findViewById(R.id.continentTextView);
+
+        MyCountryArrayAdapter adapter
     }
 }
---
+++ app/src/main/java/com/example/myconastat/MyCountryArrayAdapter.java
@@ -0,0 +1,14 @@
+package com.example.myconastat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCountryArrayAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_country_array_adapter);
+    }
+}
